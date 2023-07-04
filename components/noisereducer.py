972546d5006cb1711e56210e69bb7acf098c68ce
ivanshin <i.shingel@outlook.com>
2023-07-04 15:31:55 +0300

    TODO: add transcriber

--- components/noisereducer.py
+++ components/noisereducer.py
@@ -14,11 +14,19 @@ def reduce_noise(path_to_audio_file: Union[Text,Path], output_dir: Union[Text,Pa
     """ Reduce noize from single audio file """
 
     file_name = path_to_audio_file.split(os.sep)[-1] 
-    sound = AudioSegment.from_wav(path_to_audio_file)
-    sound = sound.set_channels(1)
+    sound = AudioSegment.from_file(path_to_audio_file).set_channels(1)
     #sound.export("/output/path.wav", format="wav")
     rate = sound.frame_rate
-    reduced_noise = nr.reduce_noise(y=sound, sr=rate)
-    ts = datetime.strptime(datetime.now(), "%Y-%m-%d %H:%M").replace(tzinfo=timezone.utc).timestamp()
-    wavfile.write(os.path.join(output_dir, ts + file_name), rate, reduced_noise)
+    reduced_noise = nr.reduce_noise(y=sound.get_array_of_samples(), sr=rate)
+    ts = str(datetime.timestamp(datetime.now()) * 1000).split('.')[0]
+    wavfile.write(os.path.join(output_dir, ts + "_" + file_name), rate, reduced_noise)
     return None
+
+def cleaner_worker(configs_dict, queue) -> None:
+    """ Daemon cleaner worker """
+    while True:
+        if not queue.empty():
+            f_path = queue.get()
+            reduce_noise(f_path, configs_dict['clean_audio dir'])
+            os.remove(f_path)
+        pass
