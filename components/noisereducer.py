386cb7643f2d5062df45c9f2d982dde116195005
ivanshin <i.shingel@outlook.com>
2023-07-10 15:15:13 +0300

    parameters fine-tuning

--- components/noisereducer.py
+++ components/noisereducer.py
@@ -21,7 +21,7 @@ def reduce_noise(path_to_audio_file: Union[Text,Path], output_dir: Union[Text,Pa
     file_name = path_to_audio_file.split(os.sep)[-1] 
     sound = AudioSegment.from_file(path_to_audio_file).set_channels(1)
     rate = sound.frame_rate
-    reduced_noise = nr.reduce_noise(y=sound.get_array_of_samples(), sr=rate, prop_decrease= 0.1)
+    reduced_noise = nr.reduce_noise(y=sound.get_array_of_samples(), sr=rate, prop_decrease= 0.3)
     ts = str(datetime.timestamp(datetime.now()) * 1000).split('.')[0]
     wavfile.write(os.path.join(output_dir, ts + "_" + file_name), 16000, reduced_noise)
     return None
